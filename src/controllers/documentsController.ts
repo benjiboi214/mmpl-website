db66de28e1667ba0086f97a6e1797a502e0c9bed
Ben Elliot <belliot@liveperson.com>
2021-01-14 23:07:21 +1100

    Add document list support

--- src/controllers/documentsController.ts
+++ src/controllers/documentsController.ts
@@ -6,9 +6,20 @@ import { SiteTree } from '../utils/siteTree';
 
 // Documents List Page
 const documentList = (req: Request, res: Response): void => {
+  const pageParams = SiteTree.getPage(documentsConfig.reference);
+  const children = pageParams.children;
+  const documents = children.map(reference => {
+    const document = DocumentStore.getContent(reference);
+    document.fullPath = SiteTree.getURLPath(reference);
+    return document;
+  });
+
+  console.log(documents);
+
   res.render(documentsConfig.reference, {
     metaTitle: documentsConfig.label,
-    breadcrumbs: SiteTree.getBreadcrumbs(documentsConfig.reference)
+    breadcrumbs: SiteTree.getBreadcrumbs(documentsConfig.reference),
+    documents: documents
   });
 };
 const documentsConfig = {
@@ -26,10 +37,8 @@ SiteTree.registerPage(documentsConfig);
 
 // Documents Detail Page
 const documentDetail = (req: Request, res: Response): void => {
-  // console.log(req.route);
   const pathArray = req.route.path.split('/');
   const reference = pathArray[pathArray.length - 2];
-
   const pageParams = SiteTree.getPage(reference);
 
   res.render('document-detail', {
