eda83ec0ec20a5cbb853a83a0ed6e0910121e96b
Ben Elliot <belliot@liveperson.com>
2021-01-14 13:43:09 +1100

    MIgrate to use the SiteTree class

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
+import { SiteTree } from './utils/siteTree';
 import { router } from './routes';
+import meta from './content/meta';
+import socials from './content/socials';
 
 // Create Express server
 const app = express();
@@ -18,6 +21,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   express.static(path.join(__dirname, '../public'), { maxAge: 31557600000 })
 );
+app.use(function (req, res, next) {
+  res.locals.headerMenu = SiteTree.getHeaderMenu();
+  res.locals.footerMenu = SiteTree.getFooterMenu();
+  res.locals.content = {
+    meta,
+    socials,
+  };
+  next();
+});
 
 // Primary Routes
 app.use('', router);
