d3b20c3e5ff3a6bf8df0fc47c1559d3bc4046dfb
Ben Elliot <belliot@liveperson.com>
2021-01-14 20:52:01 +1100

    Notes for site structure

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -20,3 +20,28 @@ const homeConfig = {
   pageController: home
 };
 SiteTree.registerPage(homeConfig);
+
+/**
+*
+ * Home
+ * - Sidebar = Upcoming Events
+ * - Sidebar = Recent News
+ * Events 
+ * - Calendar
+ *  - Pool Vic Calendar (Including PDF Export)
+ * - Event List
+ *  - Single Event ( AGM, Season Start, Presentation, ETC )
+ * About
+ * - Constitution
+ * - World Rules
+ * - Venues
+ * Documentation
+ * - Scoresheet
+ * - Team Registration (Including Digital Submission)
+ * - Player Registration (Including Digital Submission)
+ * News / Announcements
+ * - AGM 
+ * - Season Kickoff
+ * - Life Member Announcements
+ * - 
+ */
