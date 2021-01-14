e902b79de55ff8750150a87690ec0fed7084fe6e
Ben Elliot <belliot@liveperson.com>
2021-01-14 21:17:28 +1100

    Add team nomination documents

--- src/content/documents.ts
+++ src/content/documents.ts
@@ -9,28 +9,29 @@ const documents = [
     description: 'This is the scoresheet used to capture the results of the Summer and Winter MMPL seasons.',
     subtitle: 'On the night',
     subDescription: [
-      'Fill out the date, division and team names',
-      'Ensure wins and losses are marked clearly',
-      'Tally the totals',
-      'Both captains need to sign the scoresheet',
-      'Submit the details of any new players'
+      'Fill out the date, division and team names.',
+      'Ensure wins and losses are marked clearly.',
+      'Tally the totals.',
+      'Both captains need to sign the scoresheet.',
+      'Submit the details of any new players.'
     ]
   },
   {
-    reference: 'team-registration',
-    label: 'Team Registration',
-    href: 'team-registration/',
+    reference: 'team-nomination',
+    label: 'Team Nomination',
+    href: 'team-nomination/',
     parent: 'documents',
-    imageRef: '',
-    pdfRef: '',
-    description: 'This is the scoresheet used to capture the results of the Summer and Winter MMPL seasons.',
-    subtitle: 'On the night',
+    imageRef: 'media/mmpl_team_nomination.png',
+    pdfRef: 'media/mmpl_team_nomination.pdf',
+    description: 'Use this form to nominate your team for the next MMPL season.',
+    subtitle: 'Please note',
     subDescription: [
-      'Fill out the date, division and team names',
-      'Ensure wins and losses are marked clearly',
-      'Tally the totals',
-      'Both captains need to sign the scoresheet',
-      'Submit the details of any new players'
+      'It is the teams responsibility to negotiate with the venue.',
+      'Ensure that there are enough tables at the venue for the number of teams.',
+      'You need to submit a new team nomination team every season.',
+      'Five players are required to play.',
+      'If you are short players, please submit the form and contact the committee for help finding spare players.',
+      'Submit this form to the MMPL Statisitician.'
     ]
   },
   {
