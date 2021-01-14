5c631eb8c607e3cf670cb8ba31f0df9df4289f72
Ben Elliot <belliot@liveperson.com>
2021-01-14 13:44:29 +1100

    Migrate views and content to new SiteTree class

---
+++ src/content/meta.ts
@@ -0,0 +1,22 @@
+const meta = {
+    header: {
+      title: 'Melbourne Metropolitan Pool League',
+      image: 'images/mmpl-logo-100x100.png',
+    },
+    footer: {
+      content_heading: 'About our club',
+      content: `
+      Melbourne Metropolitan Pool League Inc. (MMPL) is a non-profit 
+      organisation established to promote and co-ordinate the sport 
+      of 8-Ball in the inner district of Melbourne. The MMPL conducts 
+      two seasons of competition annually, the more highly-regarded 
+      one in winter (April to September – usually about 18 rounds) 
+      and the less-serious one in summer (November to March – usually 
+      about 9 rounds), in which up to 20 separate venues participate 
+      in the Melbourne CBD and surrounding areas.
+    `,
+      copyright: '© MMPL | Ben Elliot | 2021 | All rights reserved.',
+    },
+  };
+
+export default meta;
