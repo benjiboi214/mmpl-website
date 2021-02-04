2a1c7284a2c9665e66eb9aeb7c3ad13886f95a89
Ben Elliot <belliot@liveperson.com>
2021-02-04 23:14:44 +1100

    Mkae script executable from any location

--- tendenci/static/build_n_push.sh
+++ tendenci/static/build_n_push.sh
@@ -2,6 +2,7 @@
 
 IMAGE="benjiboi214/mmpl-tendenci-static"
 VERSION="latest"
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-docker build -t ${IMAGE}:${VERSION} .
+docker build -t ${IMAGE}:${VERSION} $SCRIPT_PATH/
 docker push ${IMAGE}:${VERSION}
